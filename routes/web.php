7f852aa18d8cc31df7cb6427688c797c91180843
coderboyakash <akashchowdhury864@gmail.com>
2020-07-08 15:35:53 +0530

    test file added

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes Start
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+|--------------------------------------------------------------------------
+| Web Routes End
+|--------------------------------------------------------------------------
 */
 
 Route::get('/', function () {
@@ -18,3 +21,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test/{id}',function ($id) {
+    return view('test')->withid(10);
+});
