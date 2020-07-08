1e800dd1c6a2d12a56087313216b08d9421f49f5
coderboyakash <akashchowdhury864@gmail.com>
2020-07-08 15:41:33 +0530

    test file removed

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/test/{id}',function ($id) {
-    return view('test')->withid(10);
-});
